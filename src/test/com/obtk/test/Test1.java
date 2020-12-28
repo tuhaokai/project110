e81e74a3c3346c9e00927ec35891757f6dd66f82
tuhaokai <1178974183@qq.com>
2020-12-28 14:30:29 +0800

    Initial commit

---
+++ src/test/com/obtk/test/Test1.java
@@ -0,0 +1,16 @@
+package com.obtk.test;
+
+import org.junit.Test;
+
+/**
+ * @author Xiaobei
+ * @date 2020-12-24 09:10:40
+ */
+public class Test1 {
+    @Test
+    public void textjk(){
+        System.out.println("jjjjjj");
+    }
+
+
+}
